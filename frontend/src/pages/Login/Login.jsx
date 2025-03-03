12c9b5d00dbe3489ca7cb3c41e139a5c2636ca50
ManishSaini369 <90274365+ManishSaini369@users.noreply.github.com>
2025-03-03 11:40:02 +0530

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
       dispatch(signInStart())
 
       const res = await axios.post(
-        "http://localhost:3000/api/auth/signin",
+        "https://notebook-7c6t.onrender.com/api/auth/signin",
         { email, password },
         { withCredentials: true }
       )
